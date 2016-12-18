6be3485d392ce36c800fd8ab0bf5b7956dbe6bc4
ilei2 <ilei2@illinois.edu>
2016-12-18 16:42:13 -0600

    test connect2

--- connect2.php
+++ connect2.php
@@ -4,10 +4,13 @@ $mysql_user = "ci2fcjbx9tpzqn4y";
 $password = "kozr5qksyy4oy3wm";
 $dbname = "obj143qd1hh6c964";
 
-$conn = new PDO("mysql:host={$mysql_host};dbname={$dbname};charsest=utf8mb4","{$mysql_user}", "{$password}");
+#$conn = new PDO("mysql:host={$mysql_host};dbname={$dbname};charsest=utf8mb4","{$mysql_user}", "{$password}");
+
+$conn = mysql_connect($mysql_host, $mysql_user, $password, $dbname);
+
 
 echo "Testing connect2.php";
 
-$conn = null;
+#$conn = null;
 
 ?>
