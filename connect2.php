3fc1af04749f95272cbd085f06081c93d108ed09
ilei2 <ilei2@illinois.edu>
2016-12-18 17:49:51 -0600

    test sqli

--- connect2.php
+++ connect2.php
@@ -1,15 +1,19 @@
 <?php
-$msql_host = "o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$mysql_host = "o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 $mysql_user = "ci2fcjbx9tpzqn4y";
 $password = "kozr5qksyy4oy3wm";
 $dbname = "obj143qd1hh6c964";
 
-#$conn = new PDO("mysql:host={$mysql_host};dbname={$dbname};charsest=utf8mb4","{$mysql_user}", "{$password}");
+$conn = mysqli_connect($mysql_host, $mysql_user, $password, $dbname);
+#$conn = mysql_connect($mysql_host, $mysql_user, $password, $dbname);
 
-$conn = mysql_connect($mysql_host, $mysql_user, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connection was successfully established!";
 
-echo "Testing connect2.php";
-
+#echo "Testing connect2.php";
 #$conn = null;
 
 ?>
