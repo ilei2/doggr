7ba004b5e509f28880f7d2d9a4fcdd071a374109
ilei2 <ilei2@illinois.edu>
2016-12-18 16:38:07 -0600

    testing connect2.php

---
+++ connect2.php
@@ -0,0 +1,13 @@
+<?php
+$msql_host = "o61qijqeuqnj9chh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$mysql_user = "ci2fcjbx9tpzqn4y";
+$password = "kozr5qksyy4oy3wm";
+$dbname = "obj143qd1hh6c964";
+
+$conn = new PDO("mysql:host={$mysql_host};dbname={$dbname};charsest=utf8mb4","{$mysql_user}", "{$password}");
+
+echo "Testing connect2.php";
+
+$conn = null;
+
+?>
