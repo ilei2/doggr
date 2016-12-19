9e28c923ffdfe6b0e6138935e0e35d1904e346ce
ilei2 <ilei2@illinois.edu>
2016-12-18 21:14:22 -0600

    rescue.php

--- rescue.php
+++ rescue.php
@@ -1,13 +1,14 @@
 <?php
 $string = "<html><body>";
 
-$url = getenv(“JAWSDB_URL”);
+$url = getenv("JAWSDB_URL");
 $dbparts = parse_url($url);
-$hostname = $dbparts[‘host’];
+$hostname = $dbparts['host'];
 $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
+echo "Testing connection in rescue.php";
 $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn){
   die("Connection failed: " . mysqli_connect_error());
