c11995c4240b6b99b9de06f4542c83742f61ad7b
ilei2 <ilei2@illinois.edu>
2016-12-19 19:37:36 -0600

    add ID php

--- add.php
+++ add.php
@@ -11,7 +11,6 @@ $mysql_user = 'root';
 $mysql_password = 'kija';
 $dbname = 'test';
 $conn = mysql_connect($mysql_host, $mysql_user, $mysql_password, $dbname);
-*/
 
 $url = getenv("JAWSDB_URL");
 $dbparts = parse_url($url);
@@ -19,6 +18,14 @@ $hostname = $dbparts['host'];
 $username = $dbpars['user'];
 $password = $dbpars['pass'];
 $database = ltrim($dbpars['path'],'/');
+*/
+
+$url = getenv("JAWSDB_URL");
+$dbparts = parse_url($url);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn){
