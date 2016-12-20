feb814f8d809aaf896d922bcb84201f9110b2fde
ilei2 <ilei2@illinois.edu>
2016-12-19 19:48:05 -0600

    testing

--- action_page2.php
+++ action_page2.php
@@ -13,7 +13,7 @@ $string .= shell_exec("python rec.py {$name} {$Pcity}");
 $string .= "</body></html>";
 
 echo "<center><h2>Information about {$Pname} Adoptions</h2></center>";
-echo "<center><h3>Sorted By Distance From {$Pcity} </h3></center><br>";
+//echo "<center><h3>Sorted By Distance From {$Pcity} </h3></center><br>";
 echo "<center><h3>Enter Top Three Faves</h3></center><br>";
 echo '<center><form action="add.php" method="POST">
         1st ID Pick: <input type="text" name="one" value="0"><br>
