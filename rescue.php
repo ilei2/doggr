3d28770b87849a2cc2b80c837c80eac2e0ac25b5
ilei2 <ilei2@illinois.edu>
2016-12-19 21:31:00 -0600

    update rescue files

--- rescue.php
+++ rescue.php
@@ -14,13 +14,13 @@ $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn){
   die("Connection failed: " . mysqli_connect_error());
   }
-
+/*
 if ($conn){
   $getImage = "SELECT Image FROM Dogs WHERE Name = '" . {$name} . "';";
   //$image = $conn->query($getImage);
   echo $getImage;
   $conn->close();
-}
+}*/
 
 
 //$name = $_POST["Name"];
